c5f86d4c0fe61f6a907002a1b645a682d8d30d21
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-06-06 15:49:24 -0300

    project created

---
+++ src/main/java/br/unicamp/mc851/evisita/oapisrvusersaggregator/OapiSrvUsersAggregatorApplication.java
@@ -0,0 +1,13 @@
+package br.unicamp.mc851.evisita.oapisrvusersaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OapiSrvUsersAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OapiSrvUsersAggregatorApplication.class, args);
+	}
+
+}
